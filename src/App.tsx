266092f6733591615f68a0fe96363d05ecaba423
FIRST_GUSTAV LAST_ANDERSSON <Gustavkarlandersson@hotmail.com>
2021-03-09 09:50:38 +0100

    first design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-
+import {Button} from "antd"
+import "./index.css"
 
 function App() {
   return (
     <div className="App">
-      
+      <Button type="ghost">Click me</Button>
     </div>
   );
 }
