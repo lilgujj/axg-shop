74b4ba103f5d76717cc0bb302c061ba8e3d82c04
FIRST_GUSTAV LAST_ANDERSSON <Gustavkarlandersson@hotmail.com>
2021-03-09 12:00:03 +0100

    suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Layout from "./components/Layout";
 import "./index.css"
 import { BrowserRouter  } from "react-router-dom";
+import { Http2ServerRequest } from "node:http2";
 
 function App() {
   return (
+    <Suspense fallback={<h2>Error</h2>}>
     <BrowserRouter>
       <div>
         <Layout  />
       </div>
     </BrowserRouter>
+
+    </Suspense>
   );
 }
 
