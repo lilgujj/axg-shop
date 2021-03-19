280142adb1ee6cdb33e9e36507c7c636c2847b33
antonmaenpaa <antonmaenpaa@gmail.com>
2021-03-19 12:12:21 +0100

    add shipping component

---
+++ src/components/shippingOptions.tsx
@@ -0,0 +1,36 @@
+import { ConsoleSqlOutlined } from "@ant-design/icons";
+import { Component, ContextType } from "react";
+import {CartContext} from "../context/CartContext";
+
+
+interface Props {
+    name: string,
+    price: number,
+
+}
+
+
+class ShippingOptions extends Component<Props> {
+    context!: ContextType<typeof CartContext>;
+    static contextType = CartContext;
+    
+
+    
+
+    render() {
+        const cart = this.context
+
+        return (
+            <div className="flex-col centerY centerX">
+                <h2>{this.props.name}</h2>
+                <h2>{this.props.price}kr</h2>
+                <p>{cart.total} + {this.props.price}kr</p>
+                <h3 style={{ color: 'red' }}>Total: {cart.total} + {this.props.price}kr</h3>
+                
+            </div>
+        );
+        
+    }
+}
+
+export default ShippingOptions;
